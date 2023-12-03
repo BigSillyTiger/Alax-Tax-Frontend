36dd182d11efccc25c16e8d310abffbea3cdf7d6
areos <areos.cc@gmail.com>
2023-12-03 23:53:42 +1030

    testing: deploy with heroku

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apis = axios.create({
     withCredentials: true,
-    baseURL: "http://localhost:6464",
+    baseURL: "http://https://src-backend-453b0a1d3a5a.herokuapp.com:6464",
 });
 
 export default apis;
