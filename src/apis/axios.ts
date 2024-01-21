328029b6b2c178439e3fb660b425ccc12b3ef2c1
areos <areos.cc@gmail.com>
2024-01-21 13:17:41 +1030

    update for droplet connection

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -1,9 +1,14 @@
 import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { ADDR: server_addr } = process.env;
 
 const apis = axios.create({
     withCredentials: true,
     //baseURL: "http://https://src-backend-453b0a1d3a5a.herokuapp.com:6464",
-    baseURL: "http://localhost:6464",
+    baseURL: `http://${server_addr}:6464`,
 });
 
 export default apis;
