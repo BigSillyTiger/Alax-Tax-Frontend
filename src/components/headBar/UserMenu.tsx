68e9ebc672519b72a81f23cd4c7a9770c5a6d805
areos <areos.cc@gmail.com>
2024-02-14 17:47:46 +1030

    imp: user profile icon with cap F/L name letters & random icon bg color

--- src/components/headBar/UserMenu.tsx
+++ src/components/headBar/UserMenu.tsx
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 import { ChevronDownIcon } from "@heroicons/react/20/solid";
 import { API_ADMIN } from "@/apis";
 import { useAdminStore } from "@/configs/zustore";
+import UserIcon from "./userIcon";
 
 const userNavigation = [
     { name: "Your profile", href: "#" },
@@ -27,11 +28,7 @@ const UserMenu: FC = () => {
         <Menu as="div" className="relative">
             <Menu.Button className="-m-1.5 flex items-center p-1.5">
                 <span className="sr-only">{t("sr.openHeadBarMenu")}</span>
-                <img
-                    className="h-8 w-8 rounded-full bg-gray-50"
-                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                    alt=""
-                />
+                <UserIcon fName={user.first_name} lName={user.last_name} />
                 <span className="hidden lg:flex lg:items-center">
                     <span
                         className="ml-4 text-sm font-semibold leading-6 text-gray-900"
---
+++ src/components/headBar/userIcon.tsx
@@ -0,0 +1,42 @@
+import { FC } from "react";
+
+type TuserIcon = {
+    fName: string;
+    lName: string;
+    size?: "sm" | "md" | "lg";
+};
+
+const getRandomColor = () => {
+    const iconBGColor = [
+        "bg-blue-500",
+        "bg-green-500",
+        "bg-yellow-500",
+        "bg-red-500",
+        "bg-indigo-500",
+        "bg-pink-500",
+        "bg-purple-500",
+        "bg-emerald-500",
+        "bg-teal-500",
+        "bg-fuchsia-500",
+    ];
+    return iconBGColor[Math.floor(Math.random() * iconBGColor.length)];
+};
+
+const UserIcon: FC<TuserIcon> = ({ fName, lName, size = "md" }) => {
+    const initials =
+        fName.charAt(0).toUpperCase() + lName.charAt(0).toUpperCase();
+    const sizeClass = {
+        sm: "w-6 h-6 text-base",
+        md: "w-10 h-10 text-xl",
+        lg: "w-14 h-14 text-2xl",
+    };
+    return (
+        <div
+            className={`flex items-center justify-center rounded-full text-white ${sizeClass[size]} ${getRandomColor()}`}
+        >
+            {initials}
+        </div>
+    );
+};
+
+export default UserIcon;
