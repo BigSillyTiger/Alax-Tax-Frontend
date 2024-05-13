95f879d0cec942674b8be598edc5fbd007b32b47
Areos Chen <areos.cc@gmail.com>
2024-05-13 20:07:34 +0930

    optimize: update staff checkbox with colored name

--- src/components/CheckBox.tsx
+++ src/components/CheckBox.tsx
@@ -1,4 +1,6 @@
+import { colorWithStaffUid } from "@/configs/utils/color";
 import type { ComponentPropsWithoutRef, FC } from "react";
+import { Input } from "./ui/input";
 
 type Tprops = ComponentPropsWithoutRef<"input"> & {
     name: string; //staff name
@@ -8,30 +10,31 @@ type Tprops = ComponentPropsWithoutRef<"input"> & {
 const CheckBox: FC<Tprops> = (props) => {
     const { onChange, checked, onClick, name, uid, className, ...restProps } =
         props;
+
     return (
         <div
             className={`inline-flex items-center h-16 w-auto border-2 border-gray-200 rounded-lg ${checked && "border-indigo-500"} ${className}`}
         >
-            <input
+            <Input
                 id={`link-checkbox-${uid}`}
                 type="checkbox"
                 value=""
                 onClick={onClick}
                 checked={checked}
                 onChange={onChange}
-                className={`rounded-md size-6 p-2 text-blue-600 bg-gray-200 border-gray-300 ml-2 mr-3 checked:bg-indigo-500 cursor-pointer`}
+                className={`size-6 p-2 bg-gray-200 border-gray-300 ml-2 mr-3 cursor-pointer`}
                 {...restProps}
             />
             <label
-                className={`flex flex-col items-center font-light text-gray-500 cursor-pointer select-none mr-2`}
+                className={`flex flex-col items-center font-light ${colorWithStaffUid(uid).text} cursor-pointer select-none mr-2`}
                 htmlFor={`link-checkbox-${uid}`}
             >
                 <p
-                    className={`text-lg font-bold ${checked && "text-indigo-500"}`}
+                    className={`text-lg font-bold ${checked && colorWithStaffUid(uid).text}`}
                 >
                     {name}
                 </p>
-                <p className="font-light text-sm">{uid}</p>
+                <p className="text-sm font-normal">{uid}</p>
             </label>
         </div>
     );
